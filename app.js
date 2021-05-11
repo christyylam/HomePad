1dd33562666924bb93cd94ffb311625cd4a957b4
christty <christty@students.cs.ubc.ca>
2021-05-10 20:52:45 -0700

    changes made

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const https = require("https");
+const http = require("http");
 const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 const day = date.getDate();
